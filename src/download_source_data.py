4820620f10df1ca2169efea7d2ba919796871948
ashil <aashil2495@gmail.com>
2024-04-03 11:21:35 -0700

    testing

--- src/download_source_data.py
+++ src/download_source_data.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     data_year_string=str(event['year'])
     data_month_string=str(event['month'])
     print(data_year_string)
-    
+    print(data_year_string)
     
     yellow_taxi_fullurl=default_file_url+yellow_taxi_url_part+data_year_string+'-'+data_month_string+".parquet"
     green_taxi_fullurl=default_file_url+green_taxi_url_part+data_year_string+'-'+data_month_string+".parquet"
